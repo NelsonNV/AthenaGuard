afd41da277f7a00dbe812bc05bf5c460298ff834
NelsonNV <nelsonluisnv2@gmail.com>
2023-12-14 10:27:48 -0300

    feat (model): Primeros modelos de DDBB
    se creo los modelos de base de dato un formato basico y necesario
    - vulnerabilidad: para anotar el exploit encontrados
    - target: para la empresa o lo que es nuestro objetivo
    - reporte: parainiciar la documentacion

--- athena/models.py
+++ athena/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Vugnerabilidad(models.Model):
+    cve = models.CharField(max_length=50)
+    descripcion = models.TextField()
+    critico = models.CharField(max_length=50)
+    solucion = models.TextField()
+
+class Target(models.Model):
+    nombre = models.CharField()
+    ip = models.CharField(max_length='12')
+    descripcion = models.TextField()
+
+class Reporte(models.Model):
+    vugnerabilidad = models.ForeignKey(Vugnerabilidad)
+    target = models.ForeignKey(Target)
