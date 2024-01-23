e0a2548cd5a02a77388c572bb93f7b3ac3f3a998
NelsonNV <nelsonluisnv2@gmail.com>
2024-01-22 22:11:27 -0300

    feat (backend): new urls for page and files

---
+++ athena/urls.py
@@ -0,0 +1,20 @@
+from django.urls import path, re_path
+from django.contrib.auth.views import LoginView, LogoutView
+from django.views.static import serve
+from django.conf import settings
+from .views import home, AddVulnerabilidad
+
+
+
+urlpatterns = [
+    # Paginas Basicas
+    path('login', LoginView.as_view(template_name="login.html"), name='login'),
+    path('logout', LogoutView.as_view(), name='logout'),
+    path('', home, name='home'),
+    path('vulneravilidad/add', AddVulnerabilidad, name='addVuln'),
+
+]
+urlpatterns += [
+    re_path(r'^media/(?P<path>.*)$', serve,
+            {'document_root': settings.MEDIA_ROOT})
+]
