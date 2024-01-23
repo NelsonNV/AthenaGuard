7474ee42d271f4ed930d21e0664c8af04e44f142
NelsonNV <nelsonluisnv2@gmail.com>
2024-01-22 22:24:11 -0300

    refactor(models): Rename Models

--- athena/models.py
+++ athena/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-class Vugnerabilidad(models.Model):
+class Vulnerabilidad(models.Model):
     cve = models.CharField(max_length=50)
     descripcion = models.TextField()
     critico = models.CharField(max_length=50)
@@ -12,5 +12,5 @@ class Target(models.Model):
     descripcion = models.TextField()
 
 class Reporte(models.Model):
-    vugnerabilidad = models.ForeignKey(Vugnerabilidad,on_delete=models.CASCADE)
+    vugnerabilidad = models.ForeignKey(Vulnerabilidad,on_delete=models.CASCADE)
     target = models.ForeignKey(Target,on_delete=models.CASCADE)
