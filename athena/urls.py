113e7ce7ad4eafcc55b2534b5af291031dfa1d0e
NelsonNV <nelsonluisnv2@gmail.com>
2024-01-22 22:21:14 -0300

    fix (view): Fix function name typo

--- athena/urls.py
+++ athena/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, re_path
 from django.contrib.auth.views import LoginView, LogoutView
 from django.views.static import serve
 from django.conf import settings
-from .views import home, AddVulnerabilidad
+from .views import home, add_vulnerabilidad
 
 
 
@@ -11,7 +11,7 @@ urlpatterns = [
     path('login', LoginView.as_view(template_name="login.html"), name='login'),
     path('logout', LogoutView.as_view(), name='logout'),
     path('', home, name='home'),
-    path('vulneravilidad/add', AddVulnerabilidad, name='addVuln'),
+    path('vulneravilidad/add', add_vulnerabilidad, name='addVuln'),
 
 ]
 urlpatterns += [
