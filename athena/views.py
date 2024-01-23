bafc146a997c7ac7c2b9748a78de3bcc976caabb
NelsonNV <nelsonluisnv2@gmail.com>
2024-01-22 22:17:36 -0300

    feat (backend) Created view for vulnerability form

--- athena/views.py
+++ athena/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from .forms import formVulnerabilidad
+from .models import Vulnerabilidad
 # Create your views here.
+def home(request):
+    vuln = Vulnerabilidad.objects.values
+    return render(request,'home.html',{'vuln':vuln})
+
+def add_vulnerabilidad(request):
+    if request.method == 'POST':
+        form = formVulnerabilidad
+        if form.is_valid():
+            form.save()
+        return redirect('home')
+    else:
+        form = formVulnerabilidad
+    return render(request, 'formulario.html',{'form':form})
