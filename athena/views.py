e0be24f83afd0314636e21601056ea4e22e38606
NelsonNV <nelsonluisnv2@gmail.com>
2024-01-23 22:36:21 -0300

    refactor(home): delete dict vuln

--- athena/views.py
+++ athena/views.py
@@ -1,14 +1,16 @@
-from django.shortcuts import render,redirect
+from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render,redirect
+from django.contrib import messages
+from django.urls import reverse
 from .forms import formVulnerabilidad
 from .models import Vulnerabilidad
 # Create your views here.
 def home(request):
-    vuln = Vulnerabilidad.objects.values
-    return render(request,'home.html',{'vuln':vuln})
+    return render(request,'home.html')
 
 def add_vulnerabilidad(request):
     if request.method == 'POST':
-        form = formVulnerabilidad
+        form = formVulnerabilidad()
         if form.is_valid():
             form.save()
         return redirect('home')
