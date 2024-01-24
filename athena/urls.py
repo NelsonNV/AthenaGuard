61b789d9fd3288d193aedd2a5afc026992d44bcd
NelsonNV <nelsonluisnv2@gmail.com>
2024-01-23 22:40:25 -0300

    feat(view):added missing CRUD fields vuln.

--- athena/urls.py
+++ athena/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, re_path
 from django.contrib.auth.views import LoginView, LogoutView
 from django.views.static import serve
 from django.conf import settings
-from .views import home, add_vulnerabilidad
+from .views import *
 
 
 
@@ -11,7 +11,11 @@ urlpatterns = [
     path('login', LoginView.as_view(template_name="login.html"), name='login'),
     path('logout', LogoutView.as_view(), name='logout'),
     path('', home, name='home'),
-    path('vulneravilidad/add', add_vulnerabilidad, name='addVuln'),
+    path('vulneravilidad/add/', add_vulnerabilidad, name='addVuln'),
+    path('vulneravilidad/list/', list_vulnerabilidad, name='listVuln'),
+    path('vulneravilidad/edit/<int:id>/', edit_vulnerabilidad, name='editVuln'),
+    path('vulneravilidad/delete/<int:id>/', delete_vulnerabilidad, name='deleteVuln'),
+
 
 ]
 urlpatterns += [
--- athena/views.py
+++ athena/views.py
@@ -17,3 +17,29 @@ def add_vulnerabilidad(request):
     else:
         form = formVulnerabilidad
     return render(request, 'formulario.html',{'form':form})
+
+def list_vulnerabilidad(request):
+    vuln = list(Vulnerabilidad.objects.values())
+    headers = [field.verbose_name for field in Vulnerabilidad._meta.fields]
+    return render(request, 'listVuln.html', {'values': vuln,'headers':headers})
+
+def edit_vulnerabilidad(request, id):
+    obj = get_object_or_404(Vulnerabilidad, id=id)
+    if request.method == "POST":
+        form = formVulnerabilidad(request.POST,instance=obj)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    else:
+        form = formVulnerabilidad(instance=obj)
+
+    return render(request, 'formulario.html', {'form': form})
+
+def delete_vulnerabilidad(request, id):
+    obj = get_object_or_404(Vulnerabilidad, id=id)
+    try:
+        obj.delete()
+        messages.success(request, 'La vulnerabilidad ha sido eliminada con éxito.')
+    except Exception as e:
+        messages.error(request, f'Error al intentar eliminar la vulnerabilidad: {str(e)}')
+    return redirect('listVuln')
