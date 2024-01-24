35043e24ffbb8369d2b9b83348d6421c7ddf843c
NelsonNV <nelsonluisnv2@gmail.com>
2024-01-23 22:42:29 -0300

    style(formVuln): tailwind field enhancement

--- athena/forms.py
+++ athena/forms.py
@@ -4,12 +4,12 @@ from .models import Vulnerabilidad
 class formVulnerabilidad(forms.ModelForm):
     class Meta:
         model = Vulnerabilidad
-        fields = ["cve","descripcion","critico","solucion"]
+        fields = ["nombre","cve","critico","descripcion","solucion"]
         CHOICES = ['leve','medio','HIGH']
         widgets = { 
-            'critico': forms.TextInput(attrs={'class': 'm-2 text-black'}),
-            'cve': forms.TextInput(attrs={'class': 'm-2 text-black'}),
-            'solucion': forms.Textarea(attrs={'class': 'm-2 text-black'}),
-            'descripcion': forms.Textarea(attrs={'class': 'm-2 text-black'}),
-
+            'nombre': forms.TextInput(attrs={'class': 'sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5 p-4 m-2 text-black w-80'}),
+            'critico': forms.Select(attrs={'class': 'sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5 p-4 m-2 text-black'}),
+            'cve': forms.TextInput(attrs={'class': 'sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5 p-4 m-2 h-10 text-black'}),
+            'solucion': forms.Textarea(attrs={'class': 'w-1/2 sm:w-full p-4 h-20 m-2 text-black'}),
+            'descripcion': forms.Textarea(attrs={'class': 'w-1/2 sm:w-full p-4 h-30 m-2 text-black'}),
         }
