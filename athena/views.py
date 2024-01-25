17121dcea3d58c93d9e32337cf1299a46c2ebcb5
NelsonNV <nelsonluisnv2@gmail.com>
2024-01-25 00:22:44 -0300

    feat(view): added CRUD Target

--- athena/views.py
+++ athena/views.py
@@ -2,8 +2,8 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render,redirect
 from django.contrib import messages
 from django.urls import reverse
-from .forms import formVulnerabilidad
-from .models import Vulnerabilidad
+from .forms import formVulnerabilidad, formTarget
+from .models import Target, Vulnerabilidad
 # Create your views here.
 def home(request):
     return render(request,'home.html')
@@ -37,6 +37,42 @@ def edit_vulnerabilidad(request, id):
 
 def delete_vulnerabilidad(request, id):
     obj = get_object_or_404(Vulnerabilidad, id=id)
+    try:
+        obj.delete()
+        messages.success(request, 'La vulnerabilidad ha sido eliminada con éxito.')
+    except Exception as e:
+        messages.error(request, f'Error al intentar eliminar la vulnerabilidad: {str(e)}')
+    return redirect('listVuln')
+
+def add_target(request):
+    if request.method == 'POST':
+        form = formTarget(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect('home')
+    else:
+        form = formTarget
+    return render(request, 'formulario.html',{'form':form})
+
+def list_target(request):
+    targ = list(Target.objects.values())
+    headers = [field.verbose_name for field in Target._meta.fields]
+    return render(request, 'listTarget.html', {'values': targ,'headers':headers})
+
+def edit_target(request, id):
+    obj = get_object_or_404(formTarget, id=id)
+    if request.method == "POST":
+        form = formTarget(request.POST,instance=obj)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    else:
+        form = formTarget(instance=obj)
+
+    return render(request, 'formulario.html', {'form': form})
+
+def delete_target(request, id):
+    obj = get_object_or_404(Target, id=id)
     try:
         obj.delete()
         messages.success(request, 'La vulnerabilidad ha sido eliminada con éxito.')
