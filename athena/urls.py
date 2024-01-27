7e18792d93e528ec905bcd014dae439c97d46547
NelsonNV <nelsonluisnv2@gmail.com>
2024-01-27 15:53:25 -0300

    style(urls): add header comments to paths

--- athena/urls.py
+++ athena/urls.py
@@ -11,14 +11,17 @@ urlpatterns = [
     path('login', LoginView.as_view(template_name="login.html"), name='login'),
     path('logout', LogoutView.as_view(), name='logout'),
     path('', home, name='home'),
+    # Vulnerabilidades
     path('vulneravilidad/add/', add_vulnerabilidad, name='addVuln'),
     path('vulneravilidad/list/', list_vulnerabilidad, name='listVuln'),
     path('vulneravilidad/edit/<int:id>/', edit_vulnerabilidad, name='editVuln'),
     path('vulneravilidad/delete/<int:id>/', delete_vulnerabilidad, name='deleteVuln'),
+    # Target
     path('target/add/', add_target, name='addTarg'),
     path('target/list/', list_target, name='listTarg'),
     path('target/edit/<int:id>/', edit_target, name='editTarg'),
     path('target/delete/<int:id>/', delete_target, name='deleteTarg'),
+    # Report
     path('report/<int:target>/', report_target , name='viewReport'),
 
 ]
