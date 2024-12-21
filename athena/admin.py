bfec01d226d57dc5719511d1b034fa67c8a60b57
NelsonNV <nelsonluisnv2@gmail.com>
2024-12-21 19:56:14 -0300

    feat(admin): adjust admin control to manage model data
    Adjusted the admin control to allow better management and manipulation
    of model data. This change improves the ability of the admin to handle
    and update the model entries efficiently.

--- athena/admin.py
+++ athena/admin.py
@@ -1,3 +1,35 @@
 from django.contrib import admin
+from .models import Target, Vulnerabilidad, Reporte, Evidencia, Servicios, Escaneo
 
-# Register your models here.
+
+class TargetAdmin(admin.ModelAdmin):
+    list_display = ("nombre", "ip", "descripcion")
+
+
+class VulnerabilidadAdmin(admin.ModelAdmin):
+    list_display = ("nombre", "cve", "descripcion", "critico", "solucion")
+
+
+class ReporteAdmin(admin.ModelAdmin):
+    list_display = ("vulnerabilidad", "target", "observacion")
+
+
+class EvidenciaAdmin(admin.ModelAdmin):
+    list_display = ("etapa", "target", "descripcion")
+
+
+class ServiciosAdmin(admin.ModelAdmin):
+    list_display = ("nombre", "descripcion")
+
+
+class EscaneoAdmin(admin.ModelAdmin):
+    list_display = ("target", "servicio", "puerto")
+    list_filter = ("servicio",)
+
+
+admin.site.register(Target, TargetAdmin)
+admin.site.register(Vulnerabilidad, VulnerabilidadAdmin)
+admin.site.register(Reporte, ReporteAdmin)
+admin.site.register(Evidencia, EvidenciaAdmin)
+admin.site.register(Servicios, ServiciosAdmin)
+admin.site.register(Escaneo, EscaneoAdmin)
