a28d3f70556383dcdefccb82bd43b45134569ca6
NelsonNV <nelsonluisnv2@gmail.com>
2024-12-21 12:26:56 -0300

    fix: bad path

--- AthenaGuard/settings.py
+++ AthenaGuard/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'AthenaGuard.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'Athena', 'template')],
+        'DIRS': [os.path.join(BASE_DIR, 'athena', 'template')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
