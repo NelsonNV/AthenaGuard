7bc650fd7fa75de1c118234da8cf2fd739992774
NelsonNV <nelsonluisnv2@gmail.com>
2024-12-21 19:58:51 -0300

    refactor(models): adjust syntax and __str__ method for better data display
    Refactored the model syntax for consistency and improved the `__str__`
    method to ensure better data display when showing model instances. This
    change enhances the readability and presentation of model data.

--- athena/models.py
+++ athena/models.py
@@ -1,10 +1,11 @@
 from django.db import models
 
+
 class Vulnerabilidad(models.Model):
     CRITICO_CHOICES = [
-        ('low', 'Low'),
-        ('medium', 'Medium'),
-        ('high', 'High'),
+        ("low", "Low"),
+        ("medium", "Medium"),
+        ("high", "High"),
     ]
 
     nombre = models.CharField(max_length=150)
@@ -13,33 +14,58 @@ class Vulnerabilidad(models.Model):
     critico = models.CharField(max_length=10, choices=CRITICO_CHOICES)
 
     solucion = models.TextField()
+
     def __str__(self):
-        return self.nombre
+        return f"{self.nombre}"
+
+
 class Target(models.Model):
     nombre = models.CharField(max_length=100)
     ip = models.CharField(max_length=12)
     descripcion = models.TextField()
+
     def __str__(self):
-        return self.nombre
+        return f"{self.nombre}"
+
 
 class Reporte(models.Model):
-    vulnerabilidad = models.ForeignKey(Vulnerabilidad,on_delete=models.CASCADE)
-    target = models.ForeignKey(Target,on_delete=models.CASCADE)
+    vulnerabilidad = models.ForeignKey(Vulnerabilidad, on_delete=models.CASCADE)
+    target = models.ForeignKey(Target, on_delete=models.CASCADE)
     observacion = models.TextField()
+
+    def __str__(self):
+        return f"{self.vulnerabilidad.nombre}"
+
+
 class Evidencia(models.Model):
     ETAPAS_REPORT = [
-        ('reconocimiento', 'Reconocimiento'),
-        ('escaneo', 'Escaneo'),
-        ('exploit', 'Exploit'),
+        ("reconocimiento", "Reconocimiento"),
+        ("escaneo", "Escaneo"),
+        ("exploit", "Exploit"),
     ]
     etapa = models.CharField(max_length=15, choices=ETAPAS_REPORT)
-    target = models.ForeignKey(Target,on_delete=models.CASCADE)
-    evidencia = models.ImageField(upload_to='evidencias/')
+    target = models.ForeignKey(Target, on_delete=models.CASCADE)
+    evidencia = models.ImageField(upload_to="evidencias/")
     descripcion = models.TextField()
+
+    def __str__(self):
+        return f"{self.etapa}, {self.target.nombre}"
+
+
 class Servicios(models.Model):
     nombre = models.CharField(max_length=100)
     descripcion = models.TextField()
+
+    def __str__(self):
+        return f"{self.nombre}"
+
+
 class Escaneo(models.Model):
-    target = models.ForeignKey(Target,on_delete=models.CASCADE)
-    servicio = models.ForeignKey(Servicios,on_delete=models.CASCADE, null=True, blank=True)
+    target = models.ForeignKey(Target, on_delete=models.CASCADE)
+    servicio = models.ForeignKey(
+        Servicios, on_delete=models.CASCADE, null=True, blank=True
+    )
     puerto = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.target.nombre}"
